1f7b17d5856ab39772fd6784e041cbf0e639f6cb
Bernardo B. Marques <bernardo.fire@gmail.com>
2012-09-23 14:13:51 -0300

    githat new methods

--- lib/githat.rb
+++ lib/githat.rb
@@ -2,14 +2,22 @@ require File.dirname(__FILE__) + '/parser'
 
 class Diff
   def initialize
-    @files = get_files
+    @files = files_info
   end
 
-  def get_files
-    names = get_names
+  def files_info
+    files = []
+    files_name.map do |name|
+      files << { name: name, diff: file_diff(name) }
+    end
+    files
   end
 
-  def get_names
+  def file_diff(file)
+    `git diff #{file}`
+  end
+
+  def files_name
     `git diff --name-only`.split /\n/
   end
 end
--- spec/githat_spec.rb
+++ spec/githat_spec.rb
@@ -1,7 +1,6 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "Githat" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+  it "Diff#get_names" do
   end
 end
