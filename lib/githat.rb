c5028e1f321132a4ad65313d29fcf23a80161529
Bernardo B. Marques <bernardo.fire@gmail.com>
2012-09-23 18:33:54 -0300

    instance_methods -> class_methods

--- lib/githat.rb
+++ lib/githat.rb
@@ -1,28 +1,36 @@
 require File.dirname(__FILE__) + '/parser'
 
 class Diff
-  def initialize
-    @files = files_infos
-  end
-
-  def files_infos
-    files = []
-    files_names.map do |name|
-      files << { name: name, diff: file_diff(name) }
+  class << self
+    def files_infos
+      files = []
+      files_names.map do |name|
+        files << { name: name, diff: file_diff(name) }
+      end
+      files
     end
-    files
-  end
 
-  def file_diff(file)
-    `git diff #{file}`
-  end
+    def parse_diff(file_info)
+      extension = file[:name].gsub /(.*\.)/, ''
+    end
 
-  def files_names
-    `git diff --name-only`.split /\n/
-  end
+    def file_diff(file)
+      `git diff #{file}`
+    end
 
-  def parse_diff(file_info)
-    extension = file[:name].gsub /(.*\.)/, ''
+    def files_names
+      `git diff --name-only`.split /\n/
+    end
+
+    def parse_with_lang(code, lang)
+    end
+
+    def parse_with_diff(code)
+    end
+
+    def process(code, lexer)
+      Pygmentize.process(code, lexer)
+    end
   end
 end
 
