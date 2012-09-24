a776b0a371d5dfa9cdf13656ddce62c0f6b2bbcb
Bernardo B. Marques <bernardo.fire@gmail.com>
2012-09-24 00:33:01 -0300

    trying to fix issue 1

--- lib/githat.rb
+++ lib/githat.rb
@@ -19,18 +19,38 @@ class Diff
     end
 
     def parse_diff(file_info)
+      # TODO: issue #1
       extension = file_info[:name].gsub /(.*\.)/, ''
       splited = split_diff(file_info[:diff])
-      parsed_head = parse_with_diff(splited[:head])
-      parsed_code = parse_with_lang(splited[:code], extension)
-      parsed_code = parse_with_diff(parsed_code)
-      parsed_head + parsed_code
+      heads, codes = splited[:heads], splited[:codes]
+      complete_file_diff = ''
+
+      (0...heads.size).each do |i|
+        parsed_head = parse_with_diff(heads[i])
+        parsed_code = parse_with_lang(codes[i], extension)
+        parsed_code = parse_with_diff(parsed_code)
+        complete_file_diff << (parsed_head + parsed_code)
+      end
+
+      complete_file_diff
     end
 
     def split_diff(diff)
-      head = diff.scan(/diff(?:.*\n){4}@@.*@@/).first
-      code = diff.gsub(/diff(?:.*\n){5}/, '').strip
-      { head: head, code: code }
+      { heads: split_heads(diff),
+        codes: split_codes(diff) }
+    end
+
+    def split_codes(diff)
+      codes = []
+      codes << diff.split(/^@@ .* @@/)
+      codes.shift
+    end
+
+    def split_heads(diff)
+      heads = []
+      heads << diff.scan(/(?:.*\n){4}@@ .* @@/).first
+      heads << diff.scan(/^@@ .* @@/).shift
+      heads.flatten
     end
 
     def file_diff(file)
